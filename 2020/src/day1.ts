2a02cb4fee1f1ba43188f86692e6dbf55ecbd41a
calebboyd <caleb.boyd@gmail.com>
2020-12-02 07:39:23 -0600

    fix: account for operand reuse on day1

--- 2020/src/day1.ts
+++ 2020/src/day1.ts
@@ -3,26 +3,27 @@ import { aoc } from './common'
 export const day1 = aoc(
   (x) => Number(x),
   function day1(numbers, partB, finish) {
-    const addends: number[] = []
-
-    const solved = numbers.some((a) => {
-      addends[0] = a
-      return numbers.find((b) => {
-        addends[1] = b
-        if (partB) {
-          return numbers.find((c) => {
-            addends[2] = c
-            return a + b + c === 2020
-          })
-        }
-        return a + b === 2020
+    const addends: number[] = [],
+      solved = numbers.some((a, i) => {
+        addends[0] = a
+        return numbers.find((b, j) => {
+          if (j === i) {
+            return false
+          }
+          addends[1] = b
+          if (partB) {
+            return numbers.find((c, k) => {
+              if (k === i || k === j) {
+                return false
+              }
+              addends[2] = c
+              return a + b + c === 2020
+            })
+          }
+          return a + b === 2020
+        })
       })
-    })
-
-    if (solved) {
-      finish(addends.reduce((acc, cur) => cur * acc))
-    } else {
-      finish(addends, true)
-    }
-  }
+    finish(solved ? addends.reduce((acc, cur) => cur * acc) : 'NOT')
+  },
+  'day1-edge'
 )
